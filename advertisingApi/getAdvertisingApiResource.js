7a1601e04fb72daa3c696948e7c31844c0835c35
lars <larsnolden@web.de>
2019-10-20 11:20:17 +0200

    resolve region from seller profile for amz api requests

--- advertisingApi/getAdvertisingApiResource.js
+++ advertisingApi/getAdvertisingApiResource.js
@@ -1,18 +1,29 @@
 import axios from 'axios';
+import db from '../db';
+
+const america = ['US', 'MX', 'CA'];
+
+function getApiEndpoint(region) {
+  if (america.includes(region)) return process.env.US_ADVERTISING_API_ENPOINT;
+  return process.env.EU_ADVERTISING_API_ENPOINT;
+}
 
 export default async function getAdvertisingApiResource(
   resource,
   accessToken,
   profileId,
-  region = 'US',
+  region,
   stateFilter
 ) {
+  //  find the correct profiles region if we don't give one as parameter
+  let regionToUse = region;
+  if (region === undefined) {
+    regionToUse = await db.sellerProfile.find({ profileId }).then(res => res[0]);
+  }
+
   const request = {
     method: 'GET',
-    baseURL:
-      region === 'US'
-        ? process.env.US_ADVERTISING_API_ENPOINT
-        : process.env.EU_ADVERTISING_API_ENPOINT,
+    baseURL: getApiEndpoint(regionToUse),
     url: `/v2/${resource}`,
     headers: {
       'Amazon-Advertising-API-ClientId': process.env.AMAZON_CLIENT_ID,
